e824a08bde6160e55c3bbe592ac02e0754247922
guannan-he <hgn15084095120@hotmail.com>
2020-09-01 09:35:28 +0800

    算法

---
+++ Project4/template.cpp
@@ -0,0 +1,44 @@
+#if 0
+
+#if 1
+#include <iostream>
+
+using namespace std;
+#endif
+
+
+
+#if 1
+
+
+
+
+
+
+
+
+
+int main(int argc, char* argv[]) {
+
+	return 0;
+}
+#endif
+
+
+
+
+
+
+
+
+
+
+
+#if 0
+int main(int argc, char* argv[]) {
+
+	return 0;
+}
+#endif
+
+#endif
---
+++ Project4/template.h
@@ -0,0 +1,7 @@
+#ifndef _CHxx_HEADER_
+#define _CHxx_HEADER_
+#include <iostream>
+
+using namespace std;
+
+#endif
