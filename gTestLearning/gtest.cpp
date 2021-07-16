1d189d96922d41e8c157d1b027090704d36451c0
guannan-he <hgn15084095120@hotmail.com>
2021-07-16 16:28:04 +0800

    gtest in vscode

--- gTestLearning/gtest.cpp
+++ gTestLearning/gtest.cpp
@@ -7,8 +7,31 @@
 int add(int a,int b){
     return a+b;
 }
-TEST(testCase,test0){
+
+int fib(int n){
+    if (n < 2){
+        return 1;
+    }
+    int n_1 = 1, n_2 = 1, res = 2;
+    while (n-- > 1){
+        res = n_1 + n_2;
+        n_2 = n_1;
+        n_1 = res;
+    }
+    return res;
+}
+
+TEST(addTestCase,functional){
+    EXPECT_EQ(add(2,6),8);
     EXPECT_EQ(add(2,3),5);
+    EXPECT_EQ(add(2,5),7);
+}
+TEST(fibTestCase,functional){
+    EXPECT_EQ(fib(0),1);
+    EXPECT_EQ(fib(1),1);
+    EXPECT_EQ(fib(2),2);
+    EXPECT_EQ(fib(3),3);
+    EXPECT_EQ(fib(8),34);
 }
 
 int main(int argc, char* argv[]){
